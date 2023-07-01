1d016d3c4aed1d7a844ad163e3a3d8f7fdeba95c
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-01 18:01:28 +0300

    Refactored the code for maintainability ans seperation of concern

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 const express = require('express');
 const logging = require("./middlewares/logging")
 const Joi = require('joi');
+const business_routes = require('./businesses/routes')
+const users_routes = require('./users/routes')
+const core_routes = require('./core/routes')
 
 const app = express();
 app.use(express.json()) // enables midle where that  converts request body to json object
 app.use(logging)
-const business = [
-    { id: 1, name: "Biz One" },
-    { id: 2, name: "Biz two" },
-    { id: 3, name: "Biz three" },
-    { id: 4, name: "Biz four" },
-]
-app.get("/businesses/", (req, res) => {
-    res.json({
-        query: req.query,
-        results: business.map(business => ({ url: '/businesses/' + business.id + "/", ...business }))
-    })
-});
-app.get("/businesses/:id", (req, res) => {
-    const bus = business.find((business) => req.params.id === String(business.id))
-    if (!bus) return res.status(404).json({ code: 404, detail: "No such business found" })
-    res.json(bus)
-});
-app.post("/businesses/", (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string()
-            .min(4)
-            .required(),
-        created: Joi.date().required()
-    })
-    const { error, value, warning } = schema.validate(req.body)
-    if (error) {
-        return res.status(400).json({ detail: error.details.map(detail => detail.message).join(';') })
-    }
-    res.send(value)
-});
+app.use("/business", business_routes)
+app.use("/users", users_routes)
+app.use("/", core_routes)
+
 const PORT = process.env.PORT || 3000
 
 
