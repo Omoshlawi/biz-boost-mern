84b0f51623e8b372fddb3e360588323c15011e97
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-01 13:08:15 +0300

    perfoming simple crud and path/query params access from request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json()) // enables midle where that  converts request body to json object
 const business = [
     { id: 1, name: "Biz One" },
     { id: 2, name: "Biz two" },
@@ -8,10 +9,22 @@ const business = [
     { id: 4, name: "Biz four" },
 ]
 app.get("/businesses/", (req, res) => {
-    res.json(business)
+    res.json({
+        query: req.query,
+        results: business.map(business => ({ url: '/businesses/' + business.id + "/", ...business }))
+    })
 });
 app.get("/businesses/:id", (req, res) => {
-    res.json(business.find((business) => req.params.id === String(business.id)))
+    const bus = business.find((business) => req.params.id === String(business.id))
+    if (!bus) res.status(404).json({ code: 404, detail: "No such business found" })
+    res.json(bus)
+});
+app.post("/businesses/", (req, res) => {
+    business.push({
+        id: business.length + 1,
+        ...req.body
+    })
+    res.json(business)
 });
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
